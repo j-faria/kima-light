d9d31bebc51c484d1d26047c6b94c350a79d6da0
João Faria <joao.faria@astro.up.pt>
2018-07-20 11:37:53 +0100

    renaming in TransitConditionalPrior

--- src/TransitConditionalPrior.cpp
+++ src/TransitConditionalPrior.cpp
@@ -8,9 +8,9 @@ using namespace std;
 using namespace DNest4;
 
 
+extern ContinuousDistribution *Pprior; // Orbital period
 extern ContinuousDistribution *Rratprior; // radius ratio
 extern ContinuousDistribution *aRprior; // a over Rstar
-extern ContinuousDistribution *Pprior; // Orbital period
 extern ContinuousDistribution *PhiTprior; // Transit time in Phase
 
 
@@ -20,105 +20,42 @@ RVConditionalPrior::RVConditionalPrior()
 
 
 void RVConditionalPrior::from_prior(RNG& rng)
-{
-    if(hyperpriors)
-    {
-        center = log_muP_prior->generate(rng);
-        width = wP_prior->generate(rng);
-        muK = exp(log_muK_prior->generate(rng));
-    }
-}
+{}
 
 double RVConditionalPrior::perturb_hyperparameters(RNG& rng)
 {
     double logH = 0.;
-
-    if(hyperpriors)
-    {
-        int which = rng.rand_int(3);
-
-        if(which == 0)
-            log_muP_prior->perturb(center, rng);
-        else if(which == 1)
-            wP_prior->perturb(width, rng);
-        else
-        {
-            muK = log(muK);
-            log_muK_prior->perturb(muK, rng);
-            muK = exp(muK);
-        }
-    }
-
     return logH;
 }
 
 // vec[0] = period
-// vec[1] = amplitude
-// vec[2] = phase
-// vec[3] = ecc
-// vec[4] = viewing angle
+// vec[1] = Rp/R*
+// vec[2] = a/R*
+// vec[3] = phi (phase of transit)
 
 double RVConditionalPrior::log_pdf(const std::vector<double>& vec) const
 {
-    if(hyperpriors)
-    {
-        if(vec[2] < 0. || vec[2] > 2.*M_PI ||
-           vec[3] < 0. || vec[3] >= 1.0 ||
-           vec[4] < 0. || vec[4] > 2.*M_PI)
-             return -1E300;
-
-        Pprior->setpars(center, width);
-        Kprior->setpars(muK);
-    }
-    else
-    {
-        if(vec[0] < 1. || vec[0] > 1E4 ||
-           vec[1] < 0. ||
-           vec[2] < 0. || vec[2] > 2.*M_PI ||
-           vec[3] < 0. || vec[3] >= 1.0 ||
-           vec[4] < 0. || vec[4] > 2.*M_PI)
-             return -1E300;
-    }
-
     return Pprior->log_pdf(vec[0]) +
-           Kprior->log_pdf(vec[1]) +
-           phiprior->log_pdf(vec[2]) +
-           eprior->log_pdf(vec[3]) +
-           wprior->log_pdf(vec[4]);
+           Rratprior->log_pdf(vec[1]) +
+           aRprior->log_pdf(vec[2]) +
+           PhiTprior->log_pdf(vec[3]);
 }
 
 void RVConditionalPrior::from_uniform(std::vector<double>& vec) const
 {
-    if(hyperpriors)
-    {
-        Pprior->setpars(center, width);
-        Kprior->setpars(muK);
-    }
-
     vec[0] = Pprior->cdf_inverse(vec[0]);
-    vec[1] = Kprior->cdf_inverse(vec[1]);
-    vec[2] = phiprior->cdf_inverse(vec[2]);
-    vec[3] = eprior->cdf_inverse(vec[3]);
-    vec[4] = wprior->cdf_inverse(vec[4]);
+    vec[1] = Rratprior->cdf_inverse(vec[1]);
+    vec[2] = aRprior->cdf_inverse(vec[2]);
+    vec[3] = PhiTprior->cdf_inverse(vec[3]);
 }
 
 void RVConditionalPrior::to_uniform(std::vector<double>& vec) const
 {
-    if(hyperpriors)
-    {
-        Pprior->setpars(center, width);
-        Kprior->setpars(muK);
-    }
-
     vec[0] = Pprior->cdf(vec[0]);
-    vec[1] = Kprior->cdf(vec[1]);
-    vec[2] = phiprior->cdf(vec[2]);
-    vec[3] = eprior->cdf(vec[3]);
-    vec[4] = wprior->cdf(vec[4]);
+    vec[1] = Rratprior->cdf(vec[1]);
+    vec[2] = aRprior->cdf(vec[2]);
+    vec[3] = PhiTprior->cdf(vec[3]);
 }
 
 void RVConditionalPrior::print(std::ostream& out) const
-{
-    if(hyperpriors)
-        out<<center<<' '<<width<<' '<<muK<<' ';
-}
+{}
